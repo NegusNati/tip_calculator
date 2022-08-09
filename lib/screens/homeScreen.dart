7c76a26b9f2cb8a8bb2dfe4335418f70b38b8a31
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-09 00:15:54 -0700

    mediaQuery and result card

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -12,19 +12,42 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04), //dynamic hight on any device 
         alignment: Alignment.center,
         color: Colors.white,
         child: ListView(
           scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.only(
-              top: 40.0, left: 20.0, right: 20.0, bottom: 20.0),
+          // padding: const EdgeInsets.only(
+          //     top: 40.0, left: 20.0, right: 20.0, bottom: 20.0), better way with midia Query
+          padding: const EdgeInsets.all(20.5),
           children: [
             Container(
               width: 150.0,
               height: 150.0,
               decoration: BoxDecoration(
-                color: Colors.grey[400],
+                color: Colors.grey[300],
                 borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Total Per Person',
+                    style: TextStyle(
+                        color: Colors.purple.shade400,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Divider(
+                    color: Colors.grey.shade300,
+                  ),
+                  Text(
+                    '\$ 90.9',
+                    style: TextStyle(
+                        color: Colors.purple.shade400,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20.0),
+                  ),
+                ],
               ),
             ),
           ],
