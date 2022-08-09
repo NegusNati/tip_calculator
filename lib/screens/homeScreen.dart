509b68d61847e1de70d10a6483eb88a5d06d9996
NATNAEL BIRHANU <negusnati@gmail.com>
2022-08-09 00:53:11 -0700

    textField & Snack bar for validation

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double _billAmount = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04), //dynamic hight on any device 
+        margin: EdgeInsets.all(MediaQuery.of(context).size.height *
+            0.04), //dynamic hight on any device
         alignment: Alignment.center,
         color: Colors.white,
         child: ListView(
@@ -25,27 +25,72 @@ class _HomeScreenState extends State<HomeScreen> {
               width: 150.0,
               height: 150.0,
               decoration: BoxDecoration(
-                color: Colors.grey[300],
+                color: Colors.grey.shade300.withOpacity(0.6),
                 borderRadius: BorderRadius.circular(20.0),
               ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total Per Person',
+                      style: TextStyle(
+                          color: Colors.purple.shade400,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Divider(
+                      color: Colors.grey.shade300.withOpacity(0.6),
+                    ),
+                    Text(
+                      '\$ 90.9',
+                      style: TextStyle(
+                          color: Colors.purple.shade400,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 20.0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                    color: Colors.blueGrey.shade100, style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(11.0),
+              ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Total Per Person',
-                    style: TextStyle(
-                        color: Colors.purple.shade400,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Divider(
-                    color: Colors.grey.shade300,
-                  ),
-                  Text(
-                    '\$ 90.9',
-                    style: TextStyle(
-                        color: Colors.purple.shade400,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 20.0),
+                  TextField(
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    style: TextStyle(color: Colors.purple.shade300),
+                    decoration: const InputDecoration(
+                      prefixText: "Bill Amount: ",
+                      prefixIcon: Icon(Icons.attach_money),
+                    ),
+                    onChanged: (String value) {
+                      try {
+                        _billAmount = double.parse(value);
+                      } catch (e) {
+                        _billAmount = 0.0;
+                        var snackBar = SnackBar(
+                          content: const Text(
+                            'Incorrect Input',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          backgroundColor:
+                              Colors.purple.shade200.withOpacity(0.6),
+                          elevation: 30.0,
+                          duration: const Duration(seconds: 1),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
                   ),
                 ],
               ),
